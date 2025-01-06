934699f1fa8025ab8be5846745fe2c738e538f4d
jrvldam <jrvldam@gmail.com>
2025-01-06 02:16:28 +0100

    just in case

--- zimfw/.zim/init.zsh
+++ zimfw/.zim/init.zsh
@@ -1,16 +1,18 @@
-zimfw() { source /Users/rodrigo/.zim/zimfw.zsh "${@}" }
-zmodule() { source /Users/rodrigo/.zim/zimfw.zsh "${@}" }
-fpath=(/Users/rodrigo/.zim/modules/git/functions /Users/rodrigo/.zim/modules/utility/functions /Users/rodrigo/.zim/modules/duration-info/functions /Users/rodrigo/.zim/modules/git-info/functions /Users/rodrigo/.zim/modules/prompt-pwd/functions ${fpath})
+# FILE AUTOMATICALLY GENERATED FROM /Users/rodrigo/.zimrc
+# EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
+
+if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.zim/zimfw.zsh" "${@}" }
+fpath=("${HOME}/.zim/modules/git/functions" "${HOME}/.zim/modules/utility/functions" "${HOME}/.zim/modules/duration-info/functions" "${HOME}/.zim/modules/git-info/functions" "${HOME}/.zim/modules/prompt-pwd/functions" ${fpath})
 autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw duration-info-precmd duration-info-preexec coalesce git-action git-info prompt-pwd
-source /Users/rodrigo/.zim/modules/environment/init.zsh
-source /Users/rodrigo/.zim/modules/git/init.zsh
-source /Users/rodrigo/.zim/modules/input/init.zsh
-source /Users/rodrigo/.zim/modules/termtitle/init.zsh
-source /Users/rodrigo/.zim/modules/utility/init.zsh
-source /Users/rodrigo/.zim/modules/duration-info/init.zsh
-source /Users/rodrigo/.zim/modules/gitster/gitster.zsh-theme
-source /Users/rodrigo/.zim/modules/zsh-completions/zsh-completions.plugin.zsh
-source /Users/rodrigo/.zim/modules/completion/init.zsh
-source /Users/rodrigo/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /Users/rodrigo/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /Users/rodrigo/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
+source "${HOME}/.zim/modules/environment/init.zsh"
+source "${HOME}/.zim/modules/git/init.zsh"
+source "${HOME}/.zim/modules/input/init.zsh"
+source "${HOME}/.zim/modules/termtitle/init.zsh"
+source "${HOME}/.zim/modules/utility/init.zsh"
+source "${HOME}/.zim/modules/duration-info/init.zsh"
+source "${HOME}/.zim/modules/gitster/gitster.zsh-theme"
+source "${HOME}/.zim/modules/zsh-completions/zsh-completions.plugin.zsh"
+source "${HOME}/.zim/modules/completion/init.zsh"
+source "${HOME}/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "${HOME}/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "${HOME}/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh"
