120a760f8b1f83b6ba8cdbab8e118d3ecfac429b
Rodrigo Vargas <rodrigo.vargas@crowdstrike.com>
2026-02-16 10:38:37 +0100

    lazyvim: update

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        go = {
-          "goimports",
-          -- "gofumpt",
-        },
-      },
-    },
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     formatters_by_ft = {
+  --       go = {
+  --         "goimports",
+  --         -- "gofumpt",
+  --       },
+  --     },
+  --   },
+  -- },
 }
